e5ed57094a6cff16d3b91654140c3f86ab6819d6
EvilRedHorse <39168275+EvilRedHorse@users.noreply.github.com>
2023-01-02 01:36:27 -0500

    Update release.sh
    ### move older builds to new folder, appending move time in Nanoseconds since UNIX epoch to folder name
    ### including updates to build freebsd

--- release.sh
+++ release.sh
@@ -25,14 +25,14 @@ fi
 gpg --import $keyfile
 
 # setup build-time vars
-ldflags="-s -w -X 'gitlab.com/scpcorp/ScPrime/build.GitRevision=`git rev-parse --short HEAD`' -X 'gitlab.com/scpcorp/ScPrime/build.BuildTime=`date`' -X 'gitlab.com/scpcorp/ScPrime/build.ReleaseTag=${rc}'"
+ldflags="-s -w -X 'github.com/EvilRedHorse/pubaccess-node/build.GitRevision=`git rev-parse --short HEAD`' -X 'github.com/EvilRedHorse/pubaccess-node/build.BuildTime=`date`' -X 'github.com/EvilRedHorse/pubaccess-node/build.ReleaseTag=${rc}'"
 
 for arch in amd64 arm; do
 	for os in darwin linux windows; do
 
 		# We don't need ARM versions for windows or mac (just linux)
 		if [ "$arch" == "arm" ]; then
-			if [ "$os" == "windows" ] || [ "$os" == "darwin" ]; then
+			if [ "$os" == "windows" ] || [ "$os" == "darwin" ] || [ "$os" == "freebsd" ]; then
 				continue
 			fi
 		fi
@@ -40,8 +40,9 @@ for arch in amd64 arm; do
 		echo Packaging ${os} ${arch}...
 
 		# create workspace
-		folder=release/ScPrime-$version-$os-$arch
-		rm -rf $folder
+		folder=release/pubaccess-node-$version-$os-$arch
+		# move older builds to new folder, appending move time in Nanoseconds since UNIX epoch to folder name
+		mv -f $folder $folder$(printf "_mv_$(date +%s%N)_ns")
 		mkdir -p $folder
 		# compile and sign binaries
 		for pkg in spc spd; do
@@ -53,15 +54,14 @@ for arch in amd64 arm; do
 
 		done
 
-		# add other artifacts
-		cp -r doc LICENSE README.md $folder
+		# add other artifacts ### changing after v1.6.1
+		# cp -r LICENSE README.md $folder
 		# zip
 		(
 			cd release
-			zip -rq ScPrime-$version-$os-$arch.zip ScPrime-$version-$os-$arch
+			zip -rq pubaccess-node-$version-$os-$arch.zip pubaccess-node-$version-$os-$arch
 			# sign zip release
-			gpg --armour --output ScPrime-$version-$os-$arch.zip.asc --detach-sig ScPrime-$version-$os-$arch.zip
+			gpg --armour --output pubaccess-node-$version-$os-$arch.zip.asc --detach-sig pubaccess-node-$version-$os-$arch.zip
 		)
 	done
 done
-
