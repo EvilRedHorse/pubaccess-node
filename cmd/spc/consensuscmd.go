a7b4cfb0a86db76ede7b414b3b20eba63806b6ab
EvilRedHorse <39168275+EvilRedHorse@users.noreply.github.com>
2023-01-02 22:38:29 -0500

    Update consensuscmd.go

--- cmd/spc/consensuscmd.go
+++ cmd/spc/consensuscmd.go
@@ -7,8 +7,8 @@ import (
 	"github.com/spf13/cobra"
 	"gitlab.com/NebulousLabs/errors"
 
-	"github.com/EvilRedHorse/pubaccess-node/node/api"
-	"github.com/EvilRedHorse/pubaccess-node/types"
+	"gitlab.com/scpcorp/ScPrime/node/api"
+	"gitlab.com/scpcorp/ScPrime/types"
 )
 
 var (
